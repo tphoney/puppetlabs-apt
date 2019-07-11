e83521a138095d36b8c724afb126026ddd7b636e
tphoney <tp@puppet.com>
2019-07-11 18:39:49 +0100

    load the puppet agents settings for modulepath

--- tasks/brown.rb
+++ tasks/brown.rb
@@ -2,7 +2,6 @@
 require 'json'
 require 'open3'
 require 'puppet'
-require 'pry'
 
 def local_key(type, name)
   [type, name].join('/')
@@ -10,6 +9,9 @@ end
 
 def get_resource(type, name = nil)
   key = local_key(type, name)
+  Puppet.settings.initialize_app_defaults(Puppet::Settings.app_defaults_for_run_mode(Puppet::Util::RunMode[:user]))
+  Puppet::ApplicationSupport.push_application_context(Puppet::Util::RunMode[:user])
+  Puppet.lookup(:current_environment)
   resources = if name.nil?
                 Puppet::Resource.indirection.search(key, {})
               else
@@ -30,8 +32,8 @@ begin
   puts result
   result = get_resource('user')
   puts result
-  # result = get_resource('apt_key')
-  # puts result
+  result = get_resource('apt_key')
+  puts result
   exit 0
 rescue Puppet::Error => e
   puts({ status: 'failure', error: e.message }.to_json)
