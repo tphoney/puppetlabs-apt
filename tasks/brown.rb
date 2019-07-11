cddaba9bf5fc54cca6e2cba6d335bb14fdb1697a
tphoney <tp@puppet.com>
2019-07-11 12:37:22 +0100

    return named resource, all instances of a resource

---
+++ tasks/brown.rb
@@ -0,0 +1,39 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+require 'json'
+require 'open3'
+require 'puppet'
+require 'pry'
+
+def local_key(type, name)
+  [type, name].join('/')
+end
+
+def get_resource(type, name = nil)
+  key = local_key(type, name)
+  resources = if name.nil?
+                Puppet::Resource.indirection.search(key, {})
+              else
+                [Puppet::Resource.indirection.find(key)]
+              end
+  text = ''
+  resources.each do |resource|
+    text = text + resource.prune_parameters(parameters_to_include: @extra_params).to_manifest.force_encoding(Encoding.default_external) + "\n"
+  end
+  text
+end
+
+params = JSON.parse(STDIN.read)
+action = params['action'] unless params['action'].nil?
+
+begin
+  result = get_resource('file', '/etc/passwd')
+  puts result
+  result = get_resource('user')
+  puts result
+  # result = get_resource('apt_key')
+  # puts result
+  exit 0
+rescue Puppet::Error => e
+  puts({ status: 'failure', error: e.message }.to_json)
+  exit 1
+end
